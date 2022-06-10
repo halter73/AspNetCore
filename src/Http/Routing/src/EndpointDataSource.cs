4929f88ae367b8b40d98ea7e10e79b6549bbd436
Stephen Halter <halter73@gmail.com>
2022-06-10 14:57:51 -0700

    Make EndpointDataSource API changes

--- src/Http/Routing/src/EndpointDataSource.cs
+++ src/Http/Routing/src/EndpointDataSource.cs
@@ -1,7 +1,9 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing.Patterns;
 using Microsoft.Extensions.Primitives;
 
 namespace Microsoft.AspNetCore.Routing;
@@ -11,6 +13,11 @@ namespace Microsoft.AspNetCore.Routing;
 /// </summary>
 public abstract class EndpointDataSource
 {
+    /// <summary>
+    /// Indicates whether <see cref="GetEndpointsForGroup(RoutePattern, IEnumerable{Action{EndpointBuilder}})"/> is supported.
+    /// </summary>
+    public virtual bool SupportsGroups => false;
+
     /// <summary>
     /// Gets a <see cref="IChangeToken"/> used to signal invalidation of cached <see cref="Endpoint"/>
     /// instances.
@@ -22,4 +29,13 @@ public abstract class EndpointDataSource
     /// Returns a read-only collection of <see cref="Endpoint"/> instances.
     /// </summary>
     public abstract IReadOnlyList<Endpoint> Endpoints { get; }
+
+    /// <summary>
+    /// Get the <see cref="Endpoint"/> instances for this <see cref="EndpointDataSource"/> given the specified group <paramref name="prefix"/> and <paramref name="conventions"/>.
+    /// </summary>
+    /// <param name="prefix">The prefix passed to <see cref="EndpointRouteBuilderExtensions.MapGroup(IEndpointRouteBuilder, RoutePattern)"/>.</param>
+    /// <param name="conventions">Any convention added to the <see cref="RouteGroupBuilder"/> via <see cref="IEndpointConventionBuilder.Add(Action{EndpointBuilder})"/>.</param>
+    /// <returns>Returns a read-only collection of <see cref="Endpoint"/> instances given the specified group <paramref name="prefix"/> and <paramref name="conventions"/>.</returns>
+    public virtual IReadOnlyList<Endpoint> GetEndpointsForGroup(RoutePattern prefix, IEnumerable<Action<EndpointBuilder>> conventions)
+        => throw new NotSupportedException(Resources.MapGroups_NotSupportedByEndpointDataSrouce);
 }

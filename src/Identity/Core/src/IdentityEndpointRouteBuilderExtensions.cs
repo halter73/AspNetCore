1eb7b6dc27bc864be9aad8b944141c91c6bea7eb
Stephen Halter <halter73@gmail.com>
2023-03-16 20:03:17 -0700

    Generic fun! Require IdentityUser

--- src/Identity/Core/src/IdentityEndpointRouteBuilderExtensions.cs
+++ src/Identity/Core/src/IdentityEndpointRouteBuilderExtensions.cs
@@ -17,16 +17,35 @@ namespace Microsoft.AspNetCore.Routing;
 /// </summary>
 public static class IdentityEndpointRouteBuilderExtensions
 {
+
     /// <summary>
     /// Add endpoints for registering, logging in, and logging out using ASP.NET Core Identity.
     /// </summary>
-    /// <typeparam name="TUser">The type encapsulating the user. This should match <see cref="UserManager{TUser}"/>.</typeparam>
+    /// <typeparam name="TUser">The type encapsulating the user. This should match the generic parameter in <see cref="UserManager{TUser}"/>.</typeparam>
     /// <param name="endpoints">
     /// The <see cref="IEndpointRouteBuilder"/> to add the identity endpoints to.
     /// Call <see cref="EndpointRouteBuilderExtensions.MapGroup(IEndpointRouteBuilder, string)"/> to add a prefix to all the endpoints.
     /// </param>
     /// <returns></returns>
-    public static IEndpointConventionBuilder MapIdentity<TUser>(this IEndpointRouteBuilder endpoints) where TUser : class, new()
+    public static IEndpointConventionBuilder MapIdentity<TUser>(this IEndpointRouteBuilder endpoints)
+        where TUser : IdentityUser<string>, new()
+    {
+        return endpoints.MapIdentity<TUser, string>();
+    }
+
+    /// <summary>
+    /// Add endpoints for registering, logging in, and logging out using ASP.NET Core Identity.
+    /// </summary>
+    /// <typeparam name="TUser">The type encapsulating the user. This should match the generic parameter in <see cref="UserManager{TUser}"/>.</typeparam>
+    /// <typeparam name="TUserKey">The type encapsulating the user. This should match the generic parameter <see cref="IdentityUser{TKey}"/>.</typeparam>
+    /// <param name="endpoints">
+    /// The <see cref="IEndpointRouteBuilder"/> to add the identity endpoints to.
+    /// Call <see cref="EndpointRouteBuilderExtensions.MapGroup(IEndpointRouteBuilder, string)"/> to add a prefix to all the endpoints.
+    /// </param>
+    /// <returns></returns>
+    public static IEndpointConventionBuilder MapIdentity<TUser, TUserKey>(this IEndpointRouteBuilder endpoints)
+        where TUser : IdentityUser<TUserKey>, new()
+        where TUserKey : IEquatable<TUserKey>
     {
         // Call MapGroup yourself to get a prefix.
         var group = endpoints.MapGroup("");

049b4819c65391b881b9377ecc1bdf2cdd4ee931
Stephen Halter <halter73@gmail.com>
2023-06-08 20:48:40 -0700

    Test slowloris mitigation middleware

--- src/Servers/Kestrel/samples/SampleApp/Startup.cs
+++ src/Servers/Kestrel/samples/SampleApp/Startup.cs
@@ -5,12 +5,14 @@ using System;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.IO.Pipelines;
 using System.Net;
 using System.Net.Security;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.Connections.Features;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -57,11 +59,107 @@ public class Startup
                 + $"{Environment.NewLine}"
                 + cert);
 
+            // {new string('a', 100_000_000)}
             var response = $"hello, world{Environment.NewLine}";
-            context.Response.ContentLength = response.Length;
+            //context.Response.ContentLength = response.Length;
             context.Response.ContentType = "text/plain";
-            await context.Response.WriteAsync(response);
+            while (true)
+            {
+                await context.Response.WriteAsync(response);
+            }
         });
+    }
+
+    private class TimingPipeWriter : PipeWriter
+    {
+        private readonly ConnectionContext _connection;
+        private readonly double _minBytesPerSecond;
+        private readonly PipeWriter _inner;
+        private DateTimeOffset? _startTime;
+        private DateTimeOffset? _windowTime;
+        private long _bytesSent;
+
+        private static int _failedConnections;
+
+        public TimingPipeWriter(ConnectionContext connection, double minBytesPerSecond)
+        {
+            _connection = connection;
+            _minBytesPerSecond = minBytesPerSecond;
+            _inner = connection.Transport.Output;
+        }
+
+        public override void Advance(int bytes)
+        {
+            _bytesSent += bytes;
+            _inner.Advance(bytes);
+        }
+
+        public override void CancelPendingFlush()
+        {
+            _inner.CancelPendingFlush();
+        }
+
+        public override void Complete(Exception exception = null)
+        {
+            _inner.Complete(exception);
+        }
+
+        public override async ValueTask<FlushResult> FlushAsync(CancellationToken cancellationToken = default)
+        {
+            //Console.WriteLine("FlushAsync");
+
+            var startFlushTime = DateTimeOffset.UtcNow;
+            var startTime = _startTime ??= startFlushTime;
+            var windowTime = _windowTime ??= startFlushTime;
+
+            var flushResult = await _inner.FlushAsync(cancellationToken);
+
+            var endFlushTime = DateTimeOffset.UtcNow;
+            var timeSinceConnectionStart = endFlushTime - startTime;
+            var timeSinceWindowStart = endFlushTime - windowTime;
+
+            if (timeSinceConnectionStart > TimeSpan.FromSeconds(5) && timeSinceWindowStart > TimeSpan.FromSeconds(1))
+            {
+                if (_bytesSent / timeSinceWindowStart.TotalSeconds < _minBytesPerSecond)
+                {
+                    _connection.Abort(new ConnectionAbortedException($"The response rate was not at least {_minBytesPerSecond} bytes/sec."));
+                    Console.WriteLine($"Failed connections: {Interlocked.Increment(ref _failedConnections)}");
+                    return flushResult;
+                }
+
+                Console.WriteLine($"Rate: {_bytesSent / timeSinceWindowStart.TotalSeconds}");
+            }
+
+            Console.WriteLine($"_bytesSent: {_bytesSent}");
+            Console.WriteLine($"Rate: {_bytesSent / timeSinceWindowStart.TotalSeconds}");
+            Console.WriteLine($"timeSinceWindowStart: {timeSinceWindowStart.TotalSeconds}");
+
+            if (timeSinceWindowStart > TimeSpan.FromSeconds(5))
+            {
+                // Don't start the next window until we're sure there's still something we're trying to write.
+                _windowTime = null;
+                _bytesSent = 0;
+            }
+
+            return flushResult;
+        }
+
+        public override Memory<byte> GetMemory(int sizeHint = 0)
+        {
+            return _inner.GetMemory(sizeHint);
+        }
+
+        public override Span<byte> GetSpan(int sizeHint = 0)
+        {
+            return _inner.GetSpan(sizeHint);
+        }
+    }
+
+    private class DuplexPipe : IDuplexPipe
+    {
+        public PipeReader Input { get; set; }
+
+        public PipeWriter Output { get; set; }
     }
 
     public static Task Main(string[] args)
@@ -95,13 +193,26 @@ public class Startup
                             }
                         });
 
-                        options.Listen(IPAddress.Loopback, basePort, listenOptions =>
+                        options.Listen(IPAddress.IPv6Any, basePort, listenOptions =>
                         {
                             // Uncomment the following to enable Nagle's algorithm for this endpoint.
                             //listenOptions.NoDelay = false;
 
-                            listenOptions.UseConnectionLogging();
+                            //listenOptions.UseConnectionLogging();
+
+                            listenOptions.Use(next => connection =>
+                            {
+                                connection.Transport = new DuplexPipe
+                                {
+                                    Input = connection.Transport.Input,
+                                    Output = new TimingPipeWriter(connection, minBytesPerSecond: 240)
+                                };
+
+                                return next(connection);
+                            });
                         });
+
+                        options.Limits.MaxConcurrentConnections = 1000;
 
                         options.Listen(IPAddress.Loopback, basePort + 1, listenOptions =>
                         {
@@ -171,7 +282,7 @@ public class Startup
             })
             .ConfigureLogging((_, factory) =>
             {
-                factory.SetMinimumLevel(LogLevel.Debug);
+                factory.SetMinimumLevel(LogLevel.Error);
                 factory.AddConsole();
             })
             .ConfigureAppConfiguration((hostingContext, config) =>

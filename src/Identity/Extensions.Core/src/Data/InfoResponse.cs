7c07b21400ac8948e19350932f3cdf9f26af611a
Stephen Halter <halter73@gmail.com>
2023-08-21 15:25:17 -0700

    WIP move DTOs to Extensions.Core

--- src/Identity/Extensions.Core/src/Data/InfoResponse.cs
+++ src/Identity/Extensions.Core/src/Data/InfoResponse.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Security.Claims;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
 
 namespace Microsoft.AspNetCore.Identity.Data;
 

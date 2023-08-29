ff9054ecef2e43e1fb81993bb3a0ef412afe200c
Stephen Halter <halter73@gmail.com>
2023-08-29 12:26:32 -0700

    WIP

--- src/Identity/samples/IdentitySample.ApiEndpoints/Program.cs
+++ src/Identity/samples/IdentitySample.ApiEndpoints/Program.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.Data.Sqlite;
@@ -16,8 +17,17 @@ builder.Services.AddAuthorization();
 
 builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseSqlite(connection));
-builder.Services.AddIdentityApiEndpoints<IdentityUser>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme, bearerTokenOptions =>
+{
+    bearerTokenOptions.BearerTokenExpiration = TimeSpan.FromSeconds(1);
+});
+
+builder.Services.AddIdentityCore<IdentityUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddApiEndpoints();
+//builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+//    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
@@ -25,6 +35,10 @@ app.MapGet("/", () => "Hello, World!");
 app.MapGet("/requires-auth", (ClaimsPrincipal user) => $"Hello, {user.Identity?.Name}!").RequireAuthorization();
 
 app.MapGroup("/identity").MapIdentityApi<IdentityUser>();
+app.MapGet("/identity/logout", async context =>
+{
+    await context.SignOutAsync(IdentityConstants.ApplicationScheme);
+});
 
 app.Run();
 connection.Close();

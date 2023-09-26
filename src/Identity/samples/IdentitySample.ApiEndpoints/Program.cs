dd108ff7711ef48fdfe8bd674578653fa46290f2
Stephen Halter <halter73@gmail.com>
2023-09-25 18:47:29 -0700

    WIP /manage/info ArgumentException

--- src/Identity/samples/IdentitySample.ApiEndpoints/Program.cs
+++ src/Identity/samples/IdentitySample.ApiEndpoints/Program.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.Data.Sqlite;
@@ -9,30 +8,51 @@ using Microsoft.EntityFrameworkCore;
 
 using var connection = new SqliteConnection("DataSource=:memory:");
 connection.Open();
-
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<IdentityDbContext<IdentityUser>>(options =>
+{
+    options.UseSqlite(connection);
+});
+
+builder.Services.AddAuthentication();
 builder.Services.AddAuthorization();
 
-builder.Services.AddDbContext<ApplicationDbContext>(
-    options => options.UseSqlite(connection));
 builder.Services.AddIdentityApiEndpoints<IdentityUser>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddRoles<IdentityRole>()
+    .AddUserManager<UserManager<IdentityUser>>()
+    .AddRoleManager<RoleManager<IdentityRole>>()
+    .AddEntityFrameworkStores<IdentityDbContext<IdentityUser>>();
 
 var app = builder.Build();
+await CreateDummyData();
 
-app.MapGet("/", () => "Hello, World!");
-app.MapGet("/requires-auth", (ClaimsPrincipal user) => $"Hello, {user.Identity?.Name}!").RequireAuthorization();
-
-app.MapGroup("/identity").MapIdentityApi<IdentityUser>();
+app.MapIdentityApi<IdentityUser>();
 
 app.Run();
-connection.Close();
+return;
 
-public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+async Task CreateDummyData()
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-    {
-        Database.EnsureCreated();
-    }
+    const string email = "test@example.com";
+    const string password = "Password123!";
+    const string role1 = "admin";
+    const string role2 = "user";
+
+    // Create database 
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<IdentityDbContext<IdentityUser>>();
+    dbContext.Database.EnsureDeleted();
+    dbContext.Database.EnsureCreated();
+
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    await userManager.CreateAsync(new IdentityUser(email));
+    var user = await userManager.FindByNameAsync(email);
+    await userManager.AddPasswordAsync(user, password);
+    await userManager.SetEmailAsync(user, email);
+    await roleManager.CreateAsync(new IdentityRole(role1));
+    await roleManager.CreateAsync(new IdentityRole(role2));
+    await userManager.AddToRoleAsync(user, role1);
+    await userManager.AddToRoleAsync(user, role2);
 }

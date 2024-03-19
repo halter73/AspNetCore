7a82a192d8694ed68953058616748d61fee48aa8
Stephen Halter <halter73@gmail.com>
2024-03-18 18:40:39 -0700

    WIP 3

--- src/Components/test/E2ETest/Tests/RemoteAuthenticationTest.cs
+++ src/Components/test/E2ETest/Tests/RemoteAuthenticationTest.cs
@@ -1,9 +1,16 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics;
+using System.Reflection;
 using Microsoft.AspNetCore.Components.E2ETest.Infrastructure;
 using Microsoft.AspNetCore.Components.E2ETest.Infrastructure.ServerFixtures;
 using Microsoft.AspNetCore.E2ETesting;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Testing;
 using OpenQA.Selenium;
 using TestServer;
 using Xunit.Abstractions;
@@ -13,12 +20,27 @@ namespace Microsoft.AspNetCore.Components.E2ETest.Tests;
 public class RemoteAuthenticationTest :
     ServerTestBase<BasicTestAppServerSiteFixture<RemoteAuthenticationStartup>>
 {
+    public readonly bool TestTrimmedApps = typeof(ToggleExecutionModeServerFixture<>).Assembly
+        .GetCustomAttributes<AssemblyMetadataAttribute>()
+        .First(m => m.Key == "Microsoft.AspNetCore.E2ETesting.TestTrimmedOrMultithreadingApps")
+        .Value == "true";
+
     public RemoteAuthenticationTest(
         BrowserFixture browserFixture,
         BasicTestAppServerSiteFixture<RemoteAuthenticationStartup> serverFixture,
         ITestOutputHelper output)
         : base(browserFixture, serverFixture, output)
     {
+        //Debugger.Launch();
+
+        browserFixture.EnsureNotHeadless = true;
+
+        if (TestTrimmedApps)
+        {
+            serverFixture.ApplicationAssembly = typeof(RemoteAuthenticationStartup).Assembly;
+            serverFixture.BuildWebHostMethod = BuildPublishedWebHost;
+            serverFixture.GetContentRootMethod = GetPublishedContentRoot;
+        }
     }
 
     [Fact]
@@ -28,7 +50,37 @@ public class RemoteAuthenticationTest :
         // the OIDC endpoints will fail to authenticate the user.
         Navigate("/subdir/test-remote-authentication");
 
+        //Debugger.Launch();
         var heading = Browser.Exists(By.TagName("h1"));
+        var test = heading.Text;
         Browser.Equal("Hello, Jane Doe!", () => heading.Text);
+    }
+
+    private static IHost BuildPublishedWebHost(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureLogging((ctx, lb) =>
+            {
+                TestSink sink = new TestSink();
+                lb.AddProvider(new TestLoggerProvider(sink));
+                lb.Services.AddSingleton(sink);
+            })
+            .ConfigureWebHostDefaults(webHostBuilder =>
+            {
+                webHostBuilder.UseStartup<RemoteAuthenticationStartup>();
+                // Avoid UseStaticAssets or we won't use the trimmed, published output.
+            })
+            .Build();
+
+    private static string GetPublishedContentRoot(Assembly assembly)
+    {
+        var contentRoot = Path.Combine(AppContext.BaseDirectory, "trimmed-or-threading", assembly.GetName().Name);
+
+        if (!Directory.Exists(contentRoot))
+        {
+            throw new DirectoryNotFoundException($"Test is configured to use trimmed outputs, but trimmed outputs were not found in {contentRoot}.");
+        }
+
+        //Debugger.Launch();
+        return contentRoot;
     }
 }

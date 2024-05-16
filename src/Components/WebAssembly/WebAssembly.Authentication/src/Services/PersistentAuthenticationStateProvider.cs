c5189d0d5aceaceb30f58c8d49665df061ce68f6
Stephen Halter <halter73@gmail.com>
2024-05-15 18:18:51 -0700

    Questionable trimming suppression

--- src/Components/WebAssembly/WebAssembly.Authentication/src/Services/PersistentAuthenticationStateProvider.cs
+++ src/Components/WebAssembly/WebAssembly.Authentication/src/Services/PersistentAuthenticationStateProvider.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics.CodeAnalysis;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.Options;
@@ -17,6 +18,10 @@ internal sealed class PersistentAuthenticationStateProvider : AuthenticationStat
 
     private readonly Task<AuthenticationState> _authenticationStateTask = _defaultUnauthenticatedTask;
 
+    [UnconditionalSuppressMessage(
+        "Trimming",
+        "IL2026:Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code",
+        Justification = $"{nameof(PersistentAuthenticationStateProvider)} uses the {nameof(PersistentComponentState)} APIs to deserialize the token, which are already annotated.")]
     public PersistentAuthenticationStateProvider(PersistentComponentState state, IOptions<AuthenticationStateDeserializationOptions> options)
     {
         if (!state.TryTakeFromJson<IEnumerable<KeyValuePair<string, string>>>(PersistenceKey, out var claims) || claims is null)
